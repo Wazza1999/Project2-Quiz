0d509549f4ae845c87efdf2ca2d3aca14fee9bd5
Wazza1999 <warren.selby@hotmail.co.uk>
2024-04-23 15:19:42 +0000

    added the questions in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+const questions = [
+    {
+        question: "Which Pokémon has the highest base Speed stat?", 
+        answers: [
+            {text:"Jolteon", correct: false},
+            {text:"Electrode", correct: false},
+            {text:"Ninjask", correct: false},
+            {text:"Deoxys-Speed", correct: true},
+        ]
+    },
+    {
+        question: "Which Pokémon can learn the move Dragon Ascent through an event or special means?", 
+        answers: [
+            {text:"Rayquaza", correct: true},
+            {text:"Gyarados", correct: false},
+            {text:"Dragonite", correct: false},
+            {text:"Salamence", correct: false},
+        ]  
+    },
+    {
+        question: "Which of these Pokémon does NOT have a Mega Evolution form?", 
+        answers: [
+            {text:"Gengar", correct: false},
+            {text:"Alakasam", correct: false},
+            {text:"Arcanine", correct: true},
+            {text:"Pinsir", correct: false},
+        ]
+    },
+    {
+        question: "Which Ability prevents a Pokémon from being affected by moves that force switching out", 
+        answers: [
+            {text:"Sturdy", correct: false},
+            {text:"Suction Cups", correct: false},
+            {text:"Shadow Tag", correct: true},
+            {text:"Telepathy", correct: false},
+        ]
+    },
+    {
+        question: "Which of these Pokémon has the highest base Special Defense stat?", 
+        answers: [
+            {text:"Blissey", correct: false},
+            {text:"Shuckle", correct: true},
+            {text:"Lugia", correct: false},
+            {text:"Umbreon", correct: false},
+        ] 
+    }
+]
