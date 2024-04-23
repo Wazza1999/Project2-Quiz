5bd7cae8d0226a4cb35bee6d4208e237556da17b
Wazza1999 <warren.selby@hotmail.co.uk>
2024-04-23 15:46:11 +0000

    added currentQuestion.answers.forEach to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,50 @@
 const questions = [
     {
-        question: "Which Pokémon has the highest base Speed stat?", 
+        question: "Which Pokémon has the highest base Speed stat?",
         answers: [
-            {text:"Jolteon", correct: false},
-            {text:"Electrode", correct: false},
-            {text:"Ninjask", correct: false},
-            {text:"Deoxys-Speed", correct: true},
+            { text: "Jolteon", correct: false },
+            { text: "Electrode", correct: false },
+            { text: "Ninjask", correct: false },
+            { text: "Deoxys-Speed", correct: true },
         ]
     },
     {
-        question: "Which Pokémon can learn the move Dragon Ascent through an event or special means?", 
+        question: "Which Pokémon can learn the move Dragon Ascent through an event or special means?",
         answers: [
-            {text:"Rayquaza", correct: true},
-            {text:"Gyarados", correct: false},
-            {text:"Dragonite", correct: false},
-            {text:"Salamence", correct: false},
-        ]  
-    },
-    {
-        question: "Which of these Pokémon does NOT have a Mega Evolution form?", 
-        answers: [
-            {text:"Gengar", correct: false},
-            {text:"Alakasam", correct: false},
-            {text:"Arcanine", correct: true},
-            {text:"Pinsir", correct: false},
+            { text: "Rayquaza", correct: true },
+            { text: "Gyarados", correct: false },
+            { text: "Dragonite", correct: false },
+            { text: "Salamence", correct: false },
         ]
     },
     {
-        question: "Which Ability prevents a Pokémon from being affected by moves that force switching out", 
+        question: "Which of these Pokémon does NOT have a Mega Evolution form?",
         answers: [
-            {text:"Sturdy", correct: false},
-            {text:"Suction Cups", correct: false},
-            {text:"Shadow Tag", correct: true},
-            {text:"Telepathy", correct: false},
+            { text: "Gengar", correct: false },
+            { text: "Alakasam", correct: false },
+            { text: "Arcanine", correct: true },
+            { text: "Pinsir", correct: false },
         ]
     },
     {
-        question: "Which of these Pokémon has the highest base Special Defense stat?", 
+        question: "Which Ability prevents a Pokémon from being affected by moves that force switching out",
         answers: [
-            {text:"Blissey", correct: false},
-            {text:"Shuckle", correct: true},
-            {text:"Lugia", correct: false},
-            {text:"Umbreon", correct: false},
-        ] 
+            { text: "Sturdy", correct: false },
+            { text: "Suction Cups", correct: false },
+            { text: "Shadow Tag", correct: true },
+            { text: "Telepathy", correct: false },
+        ]
+    },
+    {
+        question: "Which of these Pokémon has the highest base Special Defense stat?",
+        answers: [
+            { text: "Blissey", correct: false },
+            { text: "Shuckle", correct: true },
+            { text: "Lugia", correct: false },
+            { text: "Umbreon", correct: false },
+        ]
     }
-];
+]
 
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
@@ -60,10 +60,18 @@ function startQuiz(){
     showQuestion();
 }
 
-    function showQuestion(){
-        let currentQuestion = questions[currentQuestionIndex];
-        let questionNo = currentQuestionIndex + 1;
-        questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
 
-        
-    }
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
+
+startQuiz();
