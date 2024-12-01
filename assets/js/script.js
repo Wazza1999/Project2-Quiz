4157a8ce3e793072d1ffc99875d013a67ff74d11
Warren Selby <warren.selby@hotmail.co.uk>
2024-12-01 14:32:15 +0000

    added commas that were missing in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 //Questions for the quiz//
+
 //question 1//
 const questions = [
 {
 question: 'Which Pokemon has the highest speed stat?',
 answers:[
-    {text:"Jolteon", correct: false}
-    {text:"Electrode", correct: false}
-    {text:"Ninjask", correct: false}
-    {text:"Deoxsys-Speed", correct: true}
+    {text:"Jolteon", correct: false},
+    {text:"Electrode", correct: false},
+    {text:"Ninjask", correct: false},
+    {text:"Deoxsys-Speed", correct: true},
 ]
 },
 
@@ -15,31 +16,33 @@ answers:[
 {
     question: 'Which of the following Pokemon is known as the "Electric Mouse"?',
     answers:[
-        {text:"Charmander", correct: false}
-        {text:"Bulbasaur", correct: false}
-        {text:"Pikachu", correct: true}
-        {text:"Squirtle", correct: false}
+        {text:"Charmander", correct: false},
+        {text:"Bulbasaur", correct: false},
+        {text:"Pikachu", correct: true},
+        {text:"Squirtle", correct: false},
     ]
     },
 //Question3//
 {
     question: 'Which of these Pokémon is known for its ability to learn and transform into any other Pokémon using the move Transform?',
     answers:[
-        {text:"Ditto", correct: true}
-        {text:"Mew", correct: false}
-        {text:"Porygon", correct: false}
-        {text:"Zoroark", correct: false}
+        {text:"Ditto", correct: true},
+        {text:"Mew", correct: false},
+        {text:"Porygon", correct: false},
+        {text:"Zoroark", correct: false},
     ]
     },
 //Question4//
 {
     question: 'Which of these Pokémon has a unique type combination that no other Pokémon had at the time of its introduction?',
     answers:[
-        {text:"Giratina", correct: false}
-        {text:"Aegislash", correct: false}
-        {text:"Darkrai", correct: false}
-        {text:"Volcanion", correct: true}
+        {text:"Giratina", correct: false},
+        {text:"Aegislash", correct: false},
+        {text:"Darkrai", correct: false},
+        {text:"Volcanion", correct: true},
     ]
     },
 ];
+
+
 
