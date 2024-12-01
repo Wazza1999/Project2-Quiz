a4d7c1a71e7e15b0ee19055855920d078b48f124
Warren Selby <warren.selby@hotmail.co.uk>
2024-12-01 14:19:37 +0000

    added content for first two questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,43 +7,43 @@ const questionElement = document.getElementById(question);
 //question 1//
 const questions = [
 {
-question: '',
+question: 'Which Pokemon has the highest speed stat?',
 answers:[
-    {}
-    {}
-    {}
-    {}
+    {text:"Jolteon", correct: false}
+    {text:"Electrode", correct: false}
+    {text:"Ninjask", correct: false}
+    {text:"Deoxsys-Speed", correct: true}
 ]
 },
 
 //Question 2//
 {
-    question: '',
+    question: 'Which of the following Pokemon is known as the "Electric Mouse"?',
     answers:[
-        {}
-        {}
-        {}
-        {}
+        {text:"Charmander", correct: false}
+        {text:"Bulbasaur", correct: false}
+        {text:"Pikachu", correct: true}
+        {text:"Squirtle", correct: false}
     ]
     },
 //Question3//
 {
     question: '',
     answers:[
-        {}
-        {}
-        {}
-        {}
+        {text:"", correct: true}
+        {text:"", correct: false}
+        {text:"", correct: false}
+        {text:"", correct: false}
     ]
     },
 //Question4//
 {
     question: '',
     answers:[
-        {}
-        {}
-        {}
-        {}
+        {text:"", correct: true}
+        {text:"", correct: false}
+        {text:"", correct: false}
+        {text:"", correct: false}
     ]
     },
 ];
